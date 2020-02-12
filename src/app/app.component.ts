ba43b8cd0db7a58b2c0b7fb77bbbf9bf52506f46
alfranrula95 <aruizlara95@gmail.com>
2020-02-12 21:44:57 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,31 +12,39 @@ export class AppComponent  {
 
   public downloadAsPDF() {
     //                    portrait/landscape, unidades, relacion
-    const doc = new jsPDF('p', 'px', 'a4');
+    const doc = new jsPDF('l', 'px', 'a4');
 
     var data = document.getElementById("pdfTable");
+
     html2canvas(data, {scale : 5}).then(canvas => {
-      var imgWidth = 100;
+      var imgWidth = 650;
       var pageHeight = 1980;
       var imgHeight = canvas.height * imgWidth / canvas.width;
       var heightLeft = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/jpg');
+      const contentDataURL = canvas.toDataURL('image/jpg', 1.0);
 
       const imgProps= doc.getImageProperties(contentDataURL);
       const pdfWidth = doc.internal.pageSize.getWidth();
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width
 
-      //console.log("canvas.height : ", canvas.height,  "imgWidth", imgWidth, "canvas.width", canvas.width, "imgHeight", imgHeight);
-      
+      var height = data.offsetHeight;
+      var width = data.offsetWidth;
+
+      var Height = doc.internal.pageSize.getHeight();
+      var Width = doc.internal.pageSize.getWidth();
+      var ratio = Height / Width;
+
+      console.log(doc.orientation);
+
       //                                   x, y, width, height
-      doc.addImage(contentDataURL, 'jpg', 2, 100, pdfWidth, pdfHeight);
+      doc.addImage(contentDataURL, 'jpg', 0, 0, pdfWidth, 400);
+      //doc.addImage(contentDataURL, 'jpg', 0, 0, Width-20, (ratio*Width)-20);
+      //doc.addImage(contentDataURL, 'jpg', 0, 0, imgWidth, imgHeight);
+      //doc.addImage(contentDataURL, 'jpg', 0, 0, pdfWidth, pdfHeight);
+
       doc.save('tableToPdf.pdf');
     });
-    const specialElementHandlers = {
-      '#editor': function (element, renderer) {
-        return true;
-      }
-    };
   }
 }
+
